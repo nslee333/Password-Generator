3a4166718b61450a778ac6a0ff25d7e796275a22
Nathan Lee <nslee333@gmail.com>
2021-10-14 17:42:44 -0700

    Commit 10/14/21 #2

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const characterAmountRange = document.getElementById ('characterAmountRange');
-const characterAmountNumber = document.getElementById ('characterAmountNumber');
+const characterAmountRange = document.getElementById('characterAmountRange');
+const characterAmountNumber = document.getElementById('characterAmountNumber');
+
+const form = document.getElementById("passwordGeneratorForm")
+
+const includeUpperCaseElement = document.getElementById("includeUppercase")
+const includeNumbersElement = document.getElementById("includeNumbers")
+const includeSymbolsElement = document.getElementById("includeSymbols")
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
-
-
 characterAmountRange.addEventListener ('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUpperCaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+})
 
 function syncCharacterAmount(e) {
   const value = e.target.value;
